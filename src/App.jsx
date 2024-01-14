8ceb8992be00c4bb7631016ebec1b2835ada7030
Harkirat Singh <harkirat96@gmail.com>
2024-01-14 17:27:49 +0530

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
 import { useContext, useState } from "react"
 import { CountContext } from "./context";
+import { Navigate } from "react-router-dom";
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  
+  const [count, setCount] = useState(0);  
   // wrap anyone that wants to use the teleported value inside a provider
+  // recoil, redux, Themes in mUI
   return (
     <div>
-      <CountContext.Provider value={{
-        count, setCount
-      }}>
-        <Count />
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
       </CountContext.Provider>
     </div>
   )
 }
 
-function Count() {
+function Count({setCount}) {
   return <div>
     <CountRenderer />
-    <Buttons />
+    <Buttons setCount={setCount} />
   </div>
 }
 
 function CountRenderer() {
-  const {count, setCount} = useContext(CountContext);
-
+  const count = useContext(CountContext);
   return <div>
     {count}
   </div>
 }
 
-function Buttons() {
-  const {count, setCount} = useContext(CountContext);
+function Buttons({setCount}) {
+  const count = useContext(CountContext);
   return <div>
     <button onClick={() => {
       setCount(count + 1)
--- src/context.jsx
+++ src/context.jsx
@@ -1,7 +1,4 @@
 import { createContext } from "react";
 
-export const CountContext = createContext({
-    count: 0,
-    setCount: () => {}
-});
+export const CountContext = createContext(0);
 
